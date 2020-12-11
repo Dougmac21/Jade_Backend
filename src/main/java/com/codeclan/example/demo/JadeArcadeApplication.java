c6a2e710e18a8b786f8cd0a5bf2f58536d60108a
Douglas MacIntyre <dougmac21@talk21.com>
2020-12-11 14:09:02 +0000

    Initial backend commit

---
+++ src/main/java/com/codeclan/example/demo/JadeArcadeApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JadeArcadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JadeArcadeApplication.class, args);
+	}
+
+}

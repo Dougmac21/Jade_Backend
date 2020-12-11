ce14e3959fe9965ecdaa1d84f619d0201bd19f91
Douglas MacIntyre <dougmac21@talk21.com>
2020-12-11 16:56:00 +0000

    Database Boilerplate Models

--- src/main/java/com/codeclan/example/demo/models/Player.java
+++ src/main/java/com/codeclan/example/demo/models/Player.java
@@ -1,4 +1,86 @@
 package com.codeclan.example.demo.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.hibernate.annotations.Cascade;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "players")
 public class Player {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "arcade_play_time")
+    private Integer arcade_play_time;
+
+    @ManyToMany
+    @JsonIgnoreProperties({"players"})
+    @Cascade(org.hibernate.annotations.CascadeType.SAVE_UPDATE)
+    @JoinTable(
+            name = "players_games",
+            joinColumns = {@JoinColumn(name = "player_id", nullable = false, updatable = false)},
+            inverseJoinColumns = {@JoinColumn(name = "game_id", nullable = false, updatable = false)}
+    )
+    private List<Game> games;
+
+    @OneToMany(mappedBy = "player", fetch = FetchType.LAZY)
+    @JsonIgnoreProperties(value = "players")
+    private List<Score> scores;
+
+    public Player(String name) {
+        this.name = name;
+        this.arcade_play_time = 0;
+        this.games = new ArrayList<>();
+        this.scores = new ArrayList<>();
+    }
+
+    public Player(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getArcade_play_time() {
+        return arcade_play_time;
+    }
+
+    public void setArcade_play_time(Integer arcade_play_time) {
+        this.arcade_play_time = arcade_play_time;
+    }
+
+    public List<Game> getGames() {
+        return games;
+    }
+
+    public void setGames(List<Game> games) {
+        this.games = games;
+    }
+
+    public List<Score> getScores() {
+        return scores;
+    }
+
+    public void setScores(List<Score> scores) {
+        this.scores = scores;
+    }
 }
--- src/main/java/com/codeclan/example/demo/models/Score.java
+++ src/main/java/com/codeclan/example/demo/models/Score.java
@@ -1,4 +1,81 @@
 package com.codeclan.example.demo.models;
 
+import com.codeclan.example.demo.models.Game;
+import com.codeclan.example.demo.models.Player;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "scores")
 public class Score {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "player_id", nullable = false)
+    @JsonIgnoreProperties({"scores"})
+    private Player player;
+
+    @ManyToOne
+    @JoinColumn(name = "game_id", nullable = false)
+    @JsonIgnoreProperties({"scores"})
+    private Game game;
+
+    @Column(name = "score")
+    private Integer score;
+
+    @Column(name = "date")
+    private String date;
+
+    public Score(Game game, Player player, Integer score, String name, String date) {
+        this.game = game;
+        this.player = player;
+        this.score = score;
+        this.date = date;
+    }
+
+    public Score(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
 }

fc85b13e605d7427b86c355c4c39785bf6520ad9
Douglas MacIntyre <dougmac21@talk21.com>
2020-12-17 08:20:05 +0000

    Adding logged_in_player. Controller to be written

---
+++ src/main/java/com/codeclan/example/demo/controllers/LoggedInPlayerController.java
@@ -0,0 +1,4 @@
+package com.codeclan.example.demo.controllers;
+
+public class LoggedInPlayerController {
+}
---
+++ src/main/java/com/codeclan/example/demo/models/LoggedInPlayer.java
@@ -0,0 +1,78 @@
+package com.codeclan.example.demo.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "logged_in_player")
+public class LoggedInPlayer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "arcade_play_time")
+    private Integer arcade_play_time;
+
+    @OneToMany(mappedBy = "logged_in_player", fetch = FetchType.LAZY)
+    @JsonIgnoreProperties(value = "logged_in_player")
+    private List<Score> scores;
+
+    public LoggedInPlayer(String name, String password) {
+        this.name = name;
+        this.password = password;
+        this.arcade_play_time = 0;
+        this.scores = new ArrayList<>();
+    }
+
+    public LoggedInPlayer(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getArcade_play_time() {
+        return arcade_play_time;
+    }
+
+    public void setArcade_play_time(Integer arcade_play_time) {
+        this.arcade_play_time = arcade_play_time;
+    }
+
+    public List<Score> getScores() {
+        return scores;
+    }
+
+    public void setScores(List<Score> scores) {
+        this.scores = scores;
+    }
+}
